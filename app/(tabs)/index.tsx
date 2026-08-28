78de3ebd98b020c929b41e2148e9e9b8c8083a2a
matias <matiaskibrom@gmail.com>
2026-08-28 09:07:21 +0400

    second commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import Login from '@/components/Login';
 import Main from '@/components/main';
+import Splash from '@/components/splash';
 import { UserContext } from '@/context/context';
 
 export default function HomeScreen() {
   const { userId } = useContext(UserContext);
-
+  const [slash, setSplsh] = useState(true)
+  {
+    useEffect(()=>{
+      const timer = setTimeout(()=>{
+            setSplsh(false)
+      },1000)
+    },[])
+  }
+  if (slash){
+    return <Splash/>
+  }
   if (userId === null) {
     return <Login />;
   }
